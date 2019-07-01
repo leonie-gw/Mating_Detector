a9315e445160f7584d73628f470c1999f5045d41
Leonie Ganswindt <lg@neleso.com>
2019-07-01 17:06:24 +0200

    add filter for boxes

--- common.py
+++ common.py
@@ -162,3 +162,23 @@ try:
 
 except ImportError:
     from utils.np_box_ops import iou as np_iou  # noqa
+
+def filter_boxes_inside_shape(boxes, shape):
+    """
+    Args:
+        boxes: (nx4), float
+        shape: (h, w)
+
+    Returns:
+        indices: (k, )
+        selection: (kx4)
+    """
+    assert boxes.ndim == 2, boxes.shape
+    assert len(shape) == 2, shape
+    h, w = shape
+    indices = np.where(
+        (boxes[:, 0] >= 0) &
+        (boxes[:, 1] >= 0) &
+        (boxes[:, 2] <= w) &
+        (boxes[:, 3] <= h))[0]
+    return indices, boxes[indices, :]
